bcfc62576746ba571cecfbf49440b9716fb12c66
Alagu <allagappan@gmail.com>
2013-03-22 17:29:51 +0530

    Mustache configuration and basic home

--- lib/pagoda/app.rb
+++ lib/pagoda/app.rb
@@ -4,12 +4,31 @@ require 'sinatra'
 require 'pagoda'
 require 'mustache/sinatra'
 
+
+require 'pagoda/views/layout'
+
 module Shwedagon
   class App < Sinatra::Base
     register Mustache::Sinatra
+    dir = File.dirname(File.expand_path(__FILE__))
+    set :public_folder, "#{dir}/public"
+    set :static, true
+
+    set :mustache, {
+      # Tell mustache where the Views constant lives
+      :namespace => Shwedagon,
+
+      # Mustache templates live here
+      :templates => "#{dir}/templates",
+
+      # Tell mustache where the views are
+      :views => "#{dir}/views"
+    }
 
     get '/' do
-      "Hello"
+      @drafts = ['Hello There', 'Another draft']
+      @published = ['Testing here', 'Published new']
+      mustache :home
     end
   end
 end
