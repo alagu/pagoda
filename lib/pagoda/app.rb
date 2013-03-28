06a7d8cb07c98b11a83f45e49d6bdffa26646c70
Alagu <allagappan@gmail.com>
2013-03-28 10:26:31 +0530

    Make git commit working.

--- lib/pagoda/app.rb
+++ lib/pagoda/app.rb
@@ -3,17 +3,23 @@ require 'cgi'
 require 'sinatra'
 require 'pagoda'
 require 'mustache/sinatra'
+require "sinatra/reloader"
 require 'jekyll'
 require 'jekyll-mod'
+require 'json'
+require 'grit'
 
 require 'pagoda/views/layout'
 
 module Shwedagon
   class App < Sinatra::Base
     register Mustache::Sinatra
+    register Sinatra::Reloader
+
     dir = File.dirname(File.expand_path(__FILE__))
     set :public_folder, "#{dir}/public"
     set :static, true
+    cwd = Dir.pwd
 
     set :mustache, {
       # Tell mustache where the Views constant lives
@@ -59,8 +65,9 @@ module Shwedagon
       site   = Jekyll::Site.new(config)
       post   = Jekyll::Post.new(site, site.source, '', file)
       
-      @title = post.data['title']
+      @title   = post.data['title']
       @content = post.content
+      @name    = post.name
 
 
       mustache :edit
@@ -68,6 +75,35 @@ module Shwedagon
 
     get '/new' do
       mustache :new
+    end
+
+    post '/save-post' do
+      content_type 'text/plain'
+
+      filename = params[:post][:name]
+      config = Jekyll.configuration({'source' => settings.blog})
+      site   = Jekyll::Site.new(config)
+      post   = Jekyll::Post.new(site, site.source, '', filename)
+
+
+      content  = post.data.to_yaml + "---\n"
+      content += params[:post][:content]
+
+      file = File.join(site.source, *%w[_posts], filename)
+      if File.exists? file
+        File.open(file, 'w') { |file| file.write(content)}
+      end
+
+      repo = Grit::Repo.new(settings.blog)
+      Dir.chdir(settings.blog)
+
+      repo.status.changed.keys.each do |file|
+        repo.add file
+      end
+
+      data = repo.commit_index "Changed #{filename}"
+
+       redirect '/edit/' + filename
     end
 
   end
