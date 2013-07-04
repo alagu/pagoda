a8ab4a403a405aecf82d439a65ed911c05b83f9d
Alagu <alagu@alagu.net>
2013-07-03 17:46:24 -0700

    IO Error for preclosing the file object

--- lib/pagoda/helper.rb
+++ lib/pagoda/helper.rb
@@ -93,11 +93,12 @@ module Shwedagon
     def set_ssh_access
       if ENV.has_key? 'SSH_PRIVATE_KEY' and (not File.exists? '/app/.ssh/id_rsa')
         FileUtils.mkdir '/app/.ssh/'
-        f = File.new '/app/.ssh/id_rsa', 'w+'
-        f.write ENV['SSH_PRIVATE_KEY']
-        f.close
-        f.chmod(0600)
-        FileUtils.chmod '/app/.ssh/' 
+        File.new '/app/.ssh/id_rsa', 'w+' do |f|          
+          f.write ENV['SSH_PRIVATE_KEY']
+          f.chmod(0600)
+        end
+        
+        FileUtils.chmod 0700, '/app/.ssh'
       end
     end
   end
